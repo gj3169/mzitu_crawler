477eef3c91c36349908a17e2bab3b330bf1f3479
gj3169 <gj3169@users.noreply.github.com>
2017-02-23 22:24:23 +0800

    增加自动识别功能，过一段时间可再次运行程序，自动跳过已下载的文件夹，可实现自动更新功能

--- spidermzitu.py
+++ spidermzitu.py
@@ -11,20 +11,23 @@ def getimg(baseurl, title):
     html_Soup = BeautifulSoup(html.text, 'lxml')
     max_span = html_Soup.find('div', class_='pagenavi').find_all('span')[-2].get_text()
     path = title.replace('?', '_').replace(' ', '_')
-    mkdir(path)
-    os.chdir("E:\mzitu\\"+path)
-    for page in range(1, int(max_span)+1): ##不知道为什么这么用的小哥儿去看看基础教程吧
-        page_url = baseurl + '/' + str(page) ##同上
-        #print(page_url)
-        img_html = requests.get(page_url, headers=headers)
-        img_Soup = BeautifulSoup(img_html.text, 'lxml')
-        img_url = img_Soup.find('div', class_='main-image').find('img')['src'] ##这三行上面都说过啦不解释了哦
-        #print(img_url)        
-        name = img_url[-9:-4] ##取URL 倒数第四至第九位 做图片的名字
-        img = requests.get(img_url, headers=headers)
-        f = open(name+'.jpg', 'wb')##写入多媒体文件必须要 b 这个参数！！必须要！！
-        f.write(img.content) ##多媒体文件要是用conctent哦！
-        f.close()
+    if mkdir(path):
+        os.chdir("E:\mzitu\\"+path)
+        for page in range(1, int(max_span)+1): ##不知道为什么这么用的小哥儿去看看基础教程吧
+            page_url = baseurl + '/' + str(page) ##同上
+            #print(page_url)
+            img_html = requests.get(page_url, headers=headers)
+            img_Soup = BeautifulSoup(img_html.text, 'lxml')
+            img_url = img_Soup.find('div', class_='main-image').find('img')['src'] ##这三行上面都说过啦不解释了哦
+            #print(img_url)        
+            name = img_url[-9:-4] ##取URL 倒数第四至第九位 做图片的名字
+            img = requests.get(img_url, headers=headers)
+            f = open(name+'.jpg', 'wb')##写入多媒体文件必须要 b 这个参数！！必须要！！
+            f.write(img.content) ##多媒体文件要是用conctent哦！
+            f.close()
+    else:
+        return 0
+
 def mkdir(path):
     path = path.strip()
     isExists = os.path.exists(os.path.join("E:\mzitu", path))
@@ -59,9 +62,3 @@ for pagenum in a:
         continue
     print(nexturl)          
 #datafile.close()
-    
-
-
-    
-    
-
